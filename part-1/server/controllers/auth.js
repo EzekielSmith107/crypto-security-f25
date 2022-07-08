fa1761828a4e145b98b5f95a0ef96284303b8122
Ezekiel Smith <ezesmith107@gmail.com>
2022-07-08 18:55:08 -0400

    Work in progress

--- part-1/server/controllers/auth.js
+++ part-1/server/controllers/auth.js
@@ -3,17 +3,25 @@ const bcrypt = require("bcryptjs");
 const users = []
 
 module.exports = {
+
     login: (req, res) => {
       console.log('Logging In User')
       console.log(req.body)
-      const { username, password } = req.body
+      
       for (let i = 0; i < users.length; i++) {
-        if (users[i].username === username && users[i].password === password) {
-          res.status(200).send(users[i])
-        }
+        const { username, password } = req.body
+        const existing = bcrypt.compareSync(password, users[i].passwordHash)
+
+        if (users[i].username == username && existing) {
+          let returningUser = {...users[i]};
+          delete returningUser.passwordHash;
+          return res.status(200).send(returningUser);
+        } 
       }
       res.status(400).send("User not found.")
     },
+
+
     register: (req, res) => {
         console.log('Registering User')
         console.log(req.body)
